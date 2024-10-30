8e541b1fe677745cbfb9283af74c9d714bb83aac
mattgist <matt.gist@gmail.com>
2024-10-30 13:55:14 -0600

    changing gretting banner, Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://playignaroundwithastro.netlify.app/"
+  site: "https://playignaroundwithastro.netlify.app/",
+  integrations: [preact()]
 });
-
